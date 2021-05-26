f217fc112f2d3308add9579db6d3f71bcfb0c65c
Ian Everitt <ian.andrew.everitt@gmail.com>
2021-05-26 16:24:32 -0500

    added content to server to connect to MongoDB Atlas

--- server.js
+++ server.js
@@ -7,19 +7,23 @@ const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI,{  
-    useNewUrlParser:true,
-    useFindAndModify:false
-})
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workouttracker_db',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 app.use(require("./routes/apiRoutes"));
 app.use(require("./routes/pageRoutes"));
 
-app.listen(PORT,function(){ 
+app.listen(PORT, function () {
     console.log(`App listening on Port ${PORT}`);
 });
